54bc631d31b9f560779ca8b65a4419c38178a3ca
Jophat Tamayo <tamay06131993@gmail.com>
2024-04-13 00:20:29 +0800

    return list of monthly due transaction and create monthly due transaction

--- app/controllers/monthly_dues_transactions_controller.rb
+++ app/controllers/monthly_dues_transactions_controller.rb
@@ -2,7 +2,8 @@ class MonthlyDuesTransactionsController < ApplicationController
 
     def index
         begin
-            @monthly_dues_transactions = MonthlyDueTransactionRepository.list_of_monthly_dues_transaction(subdivision.id, params)
+            @monthly_dues_transactions = MonthlyDueTransactionRepository.new(monthly_due_transaction_params, subdivision.id)
+            @monthly_dues_transactions = @monthly_dues_transactions.list_of_monthly_dues_transaction
             @monthly_due_error = false
         rescue ActiveRecord::RecordNotFound => e
             @monthly_due_error = true
@@ -14,10 +15,29 @@ class MonthlyDuesTransactionsController < ApplicationController
         begin
             @monthly_dues_transactions = MonthlyDueTransactionRepository.create_monthly_due_transaction(subdivision.id)
             @monthly_due_error = false
-        rescue ActiveRecord::RecordNotFound => e
+        rescue StandardError => e
             @monthly_due_error = true
+            @monthly_due_error_message = e
             render status: :bad_request
         end
     end
+
+    private
+
+    def monthly_due_transaction_params
+        params.permit(
+            :month,
+            :year,
+            :first_name,
+            :last_name,
+            :status,
+            :street,
+            :subdivision_uuid
+        )
+    end
+
+    # def create_monthly_due_transaction_params
+    #     params.require(:month)
+    # end
 end
  
