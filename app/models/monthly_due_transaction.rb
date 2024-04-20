3404e60e439b6bb22354b717246903222ec9e2a3
Jophat Tamayo <tamay06131993@gmail.com>
2024-04-20 15:10:21 +0800

    remove block and lot for validation and replace by year and month. remove belongs_to :subdivision

--- app/models/monthly_due_transaction.rb
+++ app/models/monthly_due_transaction.rb
@@ -3,9 +3,8 @@ class MonthlyDueTransaction < ApplicationRecord
     include StatusList
 
     belongs_to :user
-    belongs_to :subdivision
     belongs_to :monthly_due
-    validates :block, :lot, uniqueness: { scope: %i[year month user_id], message: "Transaction Exists" }
+    validates :year, :month, uniqueness: { scope: %i[year month user_id], message: "Transaction Exists" }
     validates :is_paid, :bill_amount, :user_id, :year, :month, :monthly_due_id, presence: true
 
     enum :month, {January:1, February:2, March:3, April:4, May:5, June:6, July:7, August:8, September:9, October:10, November:11, December:12}
