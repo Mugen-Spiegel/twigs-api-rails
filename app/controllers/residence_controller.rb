cce4f45c4b63fcfb7e75b9d845bc51c7f2f26d19
Jophat Tamayo <tamay06131993@gmail.com>
2024-04-20 15:09:45 +0800

    add before_action for update, add update endpoint to update information for residence

--- app/controllers/residence_controller.rb
+++ app/controllers/residence_controller.rb
@@ -1,16 +1,13 @@
 class ResidenceController < ApplicationController
 
 
-    before_action :residence, only: %i[ show ]
+    before_action :residence, only: %i[ show update]
 
     def index
         residence_repositories = ResidenceRepositories.new(params, subdivision)
         @residence =  residence_repositories.search_residence
     end
 
-    def show
-
-    end
 
     def create
       @residence = ResidenceRepositories.create(param_create, subdivision)
@@ -19,10 +16,30 @@ class ResidenceController < ApplicationController
       end
     end
 
+    def update
+        begin
+            unless @residence.update!(param_update)
+                @update_error = true
+                @update_error_message = @residence.errors
+            end
+        rescue => e
+            @update_error = true
+            @update_error_message = e
+            logger.debug e
+            logger.debug e.backtrace.join("\n")
+            
+            render status: :bad_request
+        end
+    end
+
     private
 
     def residence
-        @residence = User.find_by(uuid: params[:id])
+        begin
+            @residence = User.find_by(uuid: params[:residence_uuid])
+        rescue => e
+            render json: {error:{message:e.message}}, status: 404
+        end
     end
 
     def param_create
@@ -36,6 +53,18 @@ class ResidenceController < ApplicationController
             :email,
             :password,
             :password_confirmation
+        )
+    end
+    
+    def param_update
+        params.require(:residence).permit(
+            :first_name,
+            :middle_name,
+            :last_name,
+            :block,
+            :lot,
+            :street,
+            :email
         )
     end
 end
